addf4c37b93b1dba905e7f2abefd646f42fab3fd
Renan Mascarenhas <renanmascg@gmail.com>
2025-04-29 19:35:08 -0300

    feat: Adiciona novo cliente para gravação de dados, novas envs e atualiza a pagina de startus/id para ser um PPR corretamente

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Ping from "./Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -9,6 +11,14 @@ const View = async ({ id }: { id: string }) => {
       useCdn: false,
     })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   // TODO: Update the number of views
 
--- sanity/types.ts
+++ sanity/types.ts
@@ -229,6 +229,12 @@ export type STARTUP_BY_ID_QUERYResult = {
   image: string | null;
   pitch: string | null;
 } | null;
+// Variable: STARTUP_VIEWS_QUERY
+// Query: *[_type == "startup" && _id == $id][0] {        _id,        views,    }
+export type STARTUP_VIEWS_QUERYResult = {
+  _id: string;
+  views: number | null;
+} | null;
 
 // Query TypeMap
 import "@sanity/client";
@@ -236,5 +242,6 @@ declare module "@sanity/client" {
   interface SanityQueries {
     "\n    *[_type == \"startup\" && defined(slug.current)  && !defined($search) || title match $search || category match $search  || author -> name match $search] | order(_createdAt desc) {\n    _id,\n    title,\n    slug,\n    _createdAt,\n    author -> { _id, name, image, bio},\n    views,\n    description,\n    category,\n    image\n}": STARTUPS_QUERYResult;
     "\n    *[_type == \"startup\" && _id == $id][0] {\n        _id,\n        title,\n        slug,\n        _createdAt,\n        author -> { _id, name, username, image, bio},\n        views,\n        description,\n        category,\n        image,\n        pitch\n    }": STARTUP_BY_ID_QUERYResult;
+    "\n     *[_type == \"startup\" && _id == $id][0] {\n        _id,\n        views,\n    }\n": STARTUP_VIEWS_QUERYResult;
   }
 }
