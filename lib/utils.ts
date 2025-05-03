49fde43d549b29466ee080115572630cc4c677a8
Renan Mascarenhas <renanmascg@gmail.com>
2025-05-03 11:58:25 -0300

    feat: Adiciona pagina de criação de startups.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
 
 export function formatDate(date: string) {
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
-  }
+  };
 
-  return new Date(date).toLocaleDateString("en-US", options)
+  return new Date(date).toLocaleDateString("en-US", options);
+}
+
+export function parseServerActionResponse<T>(response: T) {
+  return JSON.parse(JSON.stringify(response));
 }
